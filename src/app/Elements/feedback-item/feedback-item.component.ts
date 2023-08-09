5dc904de7c34034b0e89419c25da9fb9917ec906
Océane Merlo <frontinfo.contact@gmail.com>
2023-08-09 09:39:19 +0200

    [ELEMENT] update the feedback-item component and add bulma's dependency

--- src/app/Elements/feedback-item/feedback-item.component.ts
+++ src/app/Elements/feedback-item/feedback-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-feedback-item',
+  selector: 'elt-feedback-item',
   templateUrl: './feedback-item.component.html',
   styleUrls: ['./feedback-item.component.sass']
 })
 export class FeedbackItemComponent {
-
+  @Input() indexInList: number = -1;
+  @Input() fullname: string = "";
+  @Input() picture_url: string = "";
+  @Input() country: string="";
+  @Input() congressParticipation: string ="";
+  @Input() specialty: string="";
+  @Input() feedback: string="";
 }
